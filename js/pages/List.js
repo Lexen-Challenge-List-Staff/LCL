5b7caa97823914bc63a3d461e5bd715524ef8368
MakingSoftwarePrograms <seif94253@gmail.com>
2025-11-06 18:15:25 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -51,10 +51,6 @@ export default {
                             <p>{{ level.id }}</p>
                         </li>
                         <li>
-                            <div class="type-title-sm">Password</div>
-                            <p>{{ level.password || 'Free to Copy' }}</p>
-                        </li>
-                        <li>
                             <div class="type-title-sm">fps</div>
                             <p>{{ level.fps || '120 FPS' }}</p>
                         </li>
