1af18c5b3130cd615fc6e89ee2ee0b46a9c9f9f4
Lexen2k <personne.ozieblo@gmail.com>
2025-11-07 20:17:55 +0100

    1 digit after decimal point

--- js/util.js
+++ js/util.js
@@ -11,7 +11,7 @@ export function embed(video) {
 
 
 export function localize(num) {
-    return num.toLocaleString(undefined, { minimumFractionDigits: 3});
+    return num.toLocaleString(undefined, { minimumFractionDigits: 1, maximumFractionDigits: 1 });
 }
 
 export function getThumbnailFromId(id) {
