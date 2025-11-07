f0c45003c5a2855c55ee7f42ced6f8b83debc121
Lexen2k <personne.ozieblo@gmail.com>
2025-11-07 15:05:13 +0100

    renabled decimal numbers

--- js/util.js
+++ js/util.js
@@ -11,7 +11,7 @@ export function embed(video) {
 
 
 export function localize(num) {
-    return num.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 });
+    return num.toLocaleString(undefined, { minimumFractionDigits: 3});
 }
 
 export function getThumbnailFromId(id) {
